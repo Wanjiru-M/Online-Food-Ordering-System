f986d87380f215a71f24fdc8c28381ed123ee1b9
wanjirumephis <wanjirunduhi@gmail.com>
2024-04-03 22:50:12 +0300

    Edit typo on fontawesome link

--- foodlist.php
+++ foodlist.php
@@ -22,7 +22,7 @@ header("location: customerlogin.php");
   </head>
 
   <link rel="stylesheet" type = "text/css" href ="css/foodlist.css">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" integrity="sha384-E6zEjhAZw60R43Xen4sOlHaC7K57D5l1aNKLiA6vMd7YAZtTvDvX1uGzZuFS/sIQ" crossorigin="anonymous">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
   <link rel="stylesheet" type = "text/css" href ="css/bootstrap.min.css">
   <script type="text/javascript" src="js/jquery.min.js"></script>
   <script type="text/javascript" src="js/bootstrap.min.js"></script>
